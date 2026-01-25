e41774c24bd139ebd8c1b04188b9d31b1b00121e
Diamond-Queen <q.neverhadthis@gmail.com>
2026-01-25 05:29:50 -0800

    installed correct dependencies

--- pages/beta/checkout.jsx
+++ pages/beta/checkout.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
-import { loadStripe } from '@stripe/js';
+import { loadStripe } from '@stripe/stripe-js';
 import {
   EmbeddedCheckoutProvider,
   EmbeddedCheckout
