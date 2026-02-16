56b427bbf95782edce2ac6a51a7887ad000c0dbb
Diamond-Queen <q.neverhadthis@gmail.com>
2026-02-15 16:48:23 -0800

    CSP errors fixed

--- proxy.ts
+++ proxy.ts
@@ -30,7 +30,7 @@ export default function handler(req: NextRequest) {
   // Content Security Policy
   response.headers.set(
     'Content-Security-Policy',
-    "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:; frame-src https://js.stripe.com;"
+    "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com blob:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https:; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https:; frame-src https://js.stripe.com; worker-src 'self' blob:;"
   );
   
   // Referrer policy
